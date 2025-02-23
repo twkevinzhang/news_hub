979017c1f81138f8f704cb0ec5c836d2b591c69c
twkevinzhang <twkevinzhang@gmail.com>
2025-02-24 06:33:10 +0800

    refac(extension-repo): use context.watch() in extensions_screen.dart

--- lib/presentation/pages/extension_repos/view/extension_repos_screen.dart
+++ lib/presentation/pages/extension_repos/view/extension_repos_screen.dart
@@ -21,43 +21,41 @@ class ExtensionReposScreen extends StatelessWidget implements AutoRouteWrapper {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<ExtensionReposCubit, ExtensionReposState>(builder: (context, state) {
-      final cubit = context.read<ExtensionReposCubit>();
-      return Scaffold(
-        appBar: AppBar(
-          leading: AutoLeadingButton(),
-          title: const Text('Extension Repos'),
+    final cubit = context.watch<ExtensionReposCubit>();
+    return Scaffold(
+      appBar: AppBar(
+        leading: AutoLeadingButton(),
+        title: const Text('Extension Repos'),
+      ),
+      body: cubit.state.repos.when(
+        initial: () => const LoadingIndicator(),
+        loading: () => const LoadingIndicator(),
+        error: (exception) => Center(
+          child: Text(exception.toString()),
         ),
-        body: state.repos.when(
-          initial: () => const LoadingIndicator(),
-          loading: () => const LoadingIndicator(),
-          error: (exception) => Center(
-            child: Text(exception.toString()),
-          ),
-          completed: (repos) => ListView.builder(
-            itemCount: repos.length,
-            itemBuilder: (context, index) {
-              final repo = repos[index];
-              return ListTile(
-                title: Text(repo.displayName),
-                subtitle: Text(repo.baseUrl),
-                trailing: IconButton(
-                  icon: const Icon(Icons.delete_outline),
-                  onPressed: () {
-                    cubit.deleteExtensionRepo(repo.baseUrl);
-                  },
-                ),
-              );
-            },
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.add_outlined),
-          onPressed: () {
-            AutoRouter.of(context).push(AddExtensionRepoRoute());
+        completed: (repos) => ListView.builder(
+          itemCount: repos.length,
+          itemBuilder: (context, index) {
+            final repo = repos[index];
+            return ListTile(
+              title: Text(repo.displayName),
+              subtitle: Text(repo.baseUrl),
+              trailing: IconButton(
+                icon: const Icon(Icons.delete_outline),
+                onPressed: () {
+                  cubit.deleteExtensionRepo(repo.baseUrl);
+                },
+              ),
+            );
           },
         ),
-      );
-    });
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add_outlined),
+        onPressed: () {
+          AutoRouter.of(context).push(AddExtensionRepoRoute());
+        },
+      ),
+    );
   }
 }
